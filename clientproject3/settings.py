0e07bc60e62cc96be01ed504fc70dff7c138ab7d
unknown <krishna>
2026-02-17 01:20:36 -0800

    hello

--- clientproject3/settings.py
+++ clientproject3/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v^^&7f720+kseta^nq!5dbre@2#77d5vv*t(^m(zbb7ri@^p(n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
